20c48ebf0c9a4d24c9fa241fdf58bdd24a0ce024
Katie Holly <git@meo.ws>
2018-05-20 00:00:02 +0200

    Initial code commit

---
+++ files/boot.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+cd /tmp
+
+initkeys() {
+	echo '$PUBLIC_KEY or $SECRET_KEY missing, proceeding with new key pairs:'
+	echo ''
+	dnscrypt-wrapper --gen-provider-keypair --provider-name=2.dnscrypt-cert.anyqa.st --ext-address=255.255.255.255 --nolog --nofilter &> /dev/null
+	PUBLIC_KEY=$(cat "./public.key" | base64 | tr -d '\n')
+	SECRET_KEY=$(cat "./secret.key" | base64 | tr -d '\n')
+	echo "PUBLIC_KEY='${PUBLIC_KEY}'"
+	echo "SECRET_KEY='${SECRET_KEY}'"
+	echo ''
+	dnscrypt-wrapper --show-provider-publickey --provider-publickey-file "./public.key"
+	echo ''
+	echo 'The keys displayed above are used to encrypt and verify messages between a DNSCrypt server and a DNSCrypt client.'
+	echo 'Losing the PUBLIC_KEY or SECRET_KEY displayed above means you have to generate new ones.'
+	echo 'Leaking the SECRET_KEY to someone else means that this someone can now do nasty stuff with it, including MITM attacks.'
+	echo 'Write down the keys above and store them at a secure place.'
+	echo 'Resuming dnscrypt-wrapper start-up process with the above generated keys in 60 seconds...'
+	sleep 60
+}
+
+test "${DNSCRYPT_PROVIDER_NAME:0:16}" != "2.dnscrypt-cert." && export DNSCRYPT_PROVIDER_NAME="2.dnscrypt-cert.${DNSCRYPT_PROVIDER_NAME}"
+
+test -z "${PUBLIC_KEY}" && initkeys
+test -z "${SECRET_KEY}" && initkeys
+
+echo "${PUBLIC_KEY}" | base64 -d > "./public.key"
+echo "${SECRET_KEY}" | base64 -d > "./secret.key"
+
+id=0
+echo "${DNSCRYPT_HOST_PORTS}" | while read host port; do
+	id="$((${id}+1))"
+	(
+		echo "[program:dnscrypt-wrapper-${id}]"
+		echo "command=/dnscrypt-wrapper.sh ${host} ${port}"
+		echo "stdout_logfile=/dev/fd/1"
+		echo "stdout_logfile_maxbytes=0"
+		echo "redirect_stderr=true"
+	) > "/etc/supervisor.d/dnscrypt-wrapper-${id}.ini"
+done
+
+exec "/usr/bin/supervisord" "-kc/etc/supervisord.conf"
---
+++ files/dnscrypt-wrapper.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+host="${1}"
+port="${2}"
+
+rollkeys() {
+	keyid="${DNSCRYPT_CERT_FILE_HISTORY_SIZE}"
+	while test "${keyid}" -gt "0"; do
+		nextkeyid="${keyid}"
+		keyid="$((${keyid}-1))"
+		test -f "${host}-${port}-${keyid}.key" && test -f "${host}-${port}-${keyid}.crt" && cp "${host}-${port}-${keyid}.key" "${host}-${port}-${nextkeyid}.key" && cp "${host}-${port}-${keyid}.crt" "${host}-${port}-${nextkeyid}.crt"
+	done
+}
+
+genkey() {
+	dnscrypt-wrapper \
+		--gen-crypt-keypair \
+		--crypt-secretkey-file="${host}-${port}-0.key" &> /dev/null
+	dnscrypt-wrapper \
+		--gen-cert-file \
+		--crypt-secretkey-file="${host}-${port}-0.key" \
+		--provider-cert-file="${host}-${port}-0.crt" \
+		--provider-publickey-file=public.key \
+		--provider-secretkey-file=secret.key \
+		--cert-file-expire-days="${DNSCRYPT_CERT_FILE_EXPIRE_DAYS}" &> /dev/null
+}
+
+if test -r "${host}-${port}-${DNSCRYPT_CERT_FILE_HISTORY_SIZE}.key"; then
+	echo "Rolling key cert pair for ${host}:${port} ..."
+	rollkeys
+else
+	echo "Rolling multiple key cert pairs for ${host}:${port} ..."
+	while ! test -r "${host}-${port}-${DNSCRYPT_CERT_FILE_HISTORY_SIZE}.key"; do
+		rollkeys
+		genkey
+	done
+fi
+
+certs=$(seq "${DNSCRYPT_CERT_FILE_HISTORY_SIZE}" | sed -r "s/^(.*)$/${host}-${port}-\1.crt/" | tr '\n' ',' | head -c-1)
+keys=$( seq "${DNSCRYPT_CERT_FILE_HISTORY_SIZE}" | sed -r "s/^(.*)$/${host}-${port}-\1.key/" | tr '\n' ',' | head -c-1)
+
+# echo \
+timeout -t "$((${DNSCRYPT_CERT_FILE_ROTATION_INTERVAL}+${DNSCRYPT_CERT_FILE_ROTATION_TIMEOUT}))" -s SIGKILL \
+timeout -t "${DNSCRYPT_CERT_FILE_ROTATION_INTERVAL}" -s SIGTERM \
+	dnscrypt-wrapper \
+		--resolver-address=127.0.0.1:53 \
+		--listen-address="${host}:${port}" \
+		--provider-name="${DNSCRYPT_PROVIDER_NAME}" \
+		--crypt-secretkey-file="${keys}" \
+		--provider-cert-file="${certs}" \
+		--unauthenticated \
+		--user=nobody
